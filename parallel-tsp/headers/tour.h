4a4074c4eca14be361ab98b8ae940d6be9093995
Matheus Cunha <matheus.cunha@g.globo>
2021-11-01 23:55:58 -0300

    add max_cities field, AddCity, GetTourMaxCities, CopyTour and PrintInfo methods for tour

--- parallel-tsp/headers/tour.h
+++ parallel-tsp/headers/tour.h
@@ -19,8 +19,17 @@ typedef struct tour_t tour;
 
 tour* CreateTour();
 
+void AddCity(tour* tour_t, int city);
+
 int* GetTourCities(tour* tour_t);
+
+int GetTourMaxCities(tour* tour_t);
 
 int GetTourNumberCities(tour* tour_t);
 
 int GetTourCost(tour* tour_t);
+
+void CopyTour(tour* dest, tour* orig);
+
+// Just for debugging
+void PrintTourInfo(tour* tour_t);
--- parallel-tsp/src/tour.c
+++ parallel-tsp/src/tour.c
@@ -21,6 +21,7 @@
 
 struct tour_t {
   int *cities;
+  int max_cities;
   int n_cities;
   int cost;
 };
@@ -30,19 +31,41 @@ tour* CreateTour(int max_cities) {
   tour *tour_t = (tour*) calloc (1, sizeof(tour));
 
   if(!tour_t) {
-    printf("Failed to create tour");
+    printf("Failed to create tour.\n");
     exit(-1);
   }
 
   tour_t->cities = (int*) calloc (max_cities, sizeof(int));
+
+  if(!tour_t->cities) {
+    printf("Failed to create cities array.\n");
+    exit(-1);
+  }
+
+  tour_t->max_cities = max_cities;
   tour_t->n_cities = 0;
   tour_t->cost = 0;
 
   return tour_t;
 }
 
+void AddCity(tour* tour_t, int city) {
+  if (tour_t->n_cities == tour_t->max_cities) {
+    printf("Tour full!\n");
+    return;
+  }
+
+  tour_t->cities[tour_t->n_cities] = city;
+  tour_t->n_cities++;
+  tour_t->cost += city;
+}
+
 int* GetTourCities(tour* tour_t) {
   return tour_t->cities;
+}
+
+int GetTourMaxCities(tour* tour_t) {
+  return tour_t->max_cities;
 }
 
 int GetTourNumberCities(tour* tour_t) {
@@ -51,4 +74,23 @@ int GetTourNumberCities(tour* tour_t) {
 
 int GetTourCost(tour* tour_t) {
   return tour_t->cost;
+}
+
+void CopyTour(tour* dest, tour* orig) {
+  for (int i=0; i < dest->max_cities; i++) {
+    dest->cities[i] = orig->cities[i];
+  }
+
+  dest->max_cities = orig->max_cities;
+  dest->n_cities = orig->n_cities;
+  dest->cost = orig->cost;
+}
+
+// Just for debugging
+void PrintTourInfo(tour* tour_t) {
+  for (int i=0; i < tour_t->max_cities; i++) {
+    printf("%d ", tour_t->cities[i]);
+  }
+
+  printf("\nNCITIES: %d\nCOST: %d\n\n", tour_t->n_cities, tour_t->cost);
 }
